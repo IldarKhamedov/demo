34a21a7331ca73809a039444848a5597570185ab
Ilham <ildarhamedov@gmail.com>
2022-03-24 13:23:34 +0300

    left

--- src/main/java/com/example/demo/GitDemo.java
+++ src/main/java/com/example/demo/GitDemo.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class GitDemo {
 
     public static void main(String[] args) {
-        System.out.println("--==22==--");
+        System.out.println("--==222==--");
     }
 
 }
