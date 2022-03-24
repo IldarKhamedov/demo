36dbb5415a7c7678f0ce453dcb9a44cf0b21cd18
Ilham <ildarhamedov@gmail.com>
2022-03-24 13:24:41 +0300

    left1

--- src/main/java/com/example/demo/GitDemo.java
+++ src/main/java/com/example/demo/GitDemo.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class GitDemo {
 
     public static void main(String[] args) {
-        System.out.println("--==222==--");
+        System.out.println("--==2222==--");
     }
 
 }
